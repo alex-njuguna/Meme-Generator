9f30dca12664ac3f610e232a321d3a7bd09eb07d
= <=>
2023-07-25 20:12:21 +0300

    meme generator

---
+++ script.js
@@ -0,0 +1,25 @@
+// https://meme-api.com/gimme/wholesomememes
+
+const generateMemeBtn = document.querySelector(".generate-meme-btn")
+const memeImage = document.querySelector(".meme-generator img")
+const memeTitle = document.querySelector(".meme-title")
+const memeAuthor = document.querySelector(".meme-author")
+
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url)
+    memeTitle.innerHTML = title
+    memeAuthor.innerHTML = `Meme by ${author}`
+}
+
+const generateMeme = () => {
+    fetch(" https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+        updateDetails(data.url, data.title, data.author)
+    })
+} 
+
+generateMemeBtn.addEventListener('click', generateMeme)
+
+generateMeme()
